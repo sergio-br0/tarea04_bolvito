67559997d5eb83bd4828735adb66449a33bc5d18
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-07-12 22:18:13 -0600

    agregar estilo boostrab a los botones y css al documento html

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html>
 <head>
   <title>Formulario Dinámico</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
   <style>
     h1{
         text-align: center;
@@ -9,25 +12,34 @@
     .error {
       border: 2px solid red;
     }
+    #ContenedorPrincipal{
+        width:30% ;
+        height:40% ;
+        margin-left: 40%;
+        margin-top: 7%;
+    }
 
   </style>
 </head>
 <body>
+<div id="ContenedorPrincipal">
   <h1><b>INGRESO DE PERSONAL</b></h1>
   
   <form id="formulario">
     <div id="contenedor">
       <div class="Grupo">
-        <input type="text" name="input[]" placeholder="Ingrese sus nonmbres">
-        <button type="button">Eliminar</button>
+        <input type="text" name="input[]" placeholder="Ingrese sus nombres">
+        <button type="button" class="btn btn-danger btn-sm">Eliminar</button>
+
       </div>
     </div>
     
-    <button type="button" id="agregarInput">Agregar Input</button>
+    <button type="button" id="agregarInput" class="btn btn-success text-white">Agregar Input</button>
     <br><br>
     
-    <button type="submit">Enviar</button>
+    <button type="submit" class="btn btn-outline-primary" >Enviar</button>
   </form>
+  </div>
   
   <script src="script.js">
     
